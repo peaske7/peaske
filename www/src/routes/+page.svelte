47c5bf1228bb0dfd4914615f47d3409d3a3f8401
peaske7 <peaske@pm.me>
2025-03-15 16:58:54 +0900

    change content

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -9,9 +9,11 @@
 	based in Tokyo.
 </p>
 <p>
-	I've been working on fintech services in Japan for the past few years using
-	Rust and Typescript.
+	For the past few years, I worked on fintech services in Japan using
+	<a href="https://youtu.be/gCXQrhVRaIc">Rust</a>
+	and Typescript, that handled transactions for millions of users.
 </p>
+<p>Today, I'm working on a Sales Tech startup in Tokyo with my friends.</p>
 <p>I like taking <a href="/photos">pictures</a> as well.</p>
 
 <style lang="postcss">
